c06b37b091cf457ead60d672b7c00b108f7d31a7
Tei-Matse <teimatse@gmail.com>
2025-05-14 22:06:17 +0000

    Created unordered lists of fruits

--- src/components/Test08.jsx
+++ src/components/Test08.jsx
@@ -7,6 +7,13 @@ Use JSX to render a <ul> list of three favorite fruits.
 function Test08() {
   return (
     // Your code here
+    <>
+      <ul>
+        <li>Banana</li>
+        <li>Pineapple</li>
+        <li>Pawpaw</li>
+      </ul>
+    </>
   );
 }
 
